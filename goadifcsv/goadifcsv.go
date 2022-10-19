c8bcd83f0b1651a72de24d12c82836d8b6dbda3f
Kenji Rikitake <kenji.rikitake@acm.org>
2022-10-19 13:23:05 +0900

    Add goadifcsv.go

---
+++ goadifcsv/goadifcsv.go
@@ -0,0 +1,97 @@
+// goadifcsv: pick up specified ADIF fields and output in CSV format
+// by Kenji Rikitake, JJ1BDX
+// Usage: goadifcsv [-f infile] [-o outfile] field_names...
+
+package main
+
+import (
+	"encoding/csv"
+	"flag"
+	"fmt"
+	"github.com/jj1bdx/adifparser"
+	"io"
+	"os"
+)
+
+func main() {
+	var infile = flag.String("f", "", "input file (stdout in none)")
+	var outfile = flag.String("o", "", "output file (stdout if none)")
+
+	var fp *os.File
+	var err error
+
+	flag.Usage = func() {
+		execname := os.Args[0]
+		fmt.Fprintln(flag.CommandLine.Output(),
+			"goadifcsv: remove specified ADIF fields")
+		fmt.Fprintf(flag.CommandLine.Output(),
+			"Usage: %s [-f infile] [-o outfile] field_names...\n", execname)
+		flag.PrintDefaults()
+	}
+
+	flag.Parse()
+
+	if *infile == "" {
+		fp = os.Stdin
+	} else {
+		fp, err = os.Open(*infile)
+		if err != nil {
+			fmt.Fprint(os.Stderr, err)
+			return
+		}
+	}
+
+	var writer *csv.Writer
+	var writefp *os.File
+	if *outfile != "" {
+		writefp, err = os.Create(*outfile)
+		if err != nil {
+			fmt.Fprint(os.Stderr, err)
+			return
+		}
+		writer = csv.NewWriter(writefp)
+	} else {
+		writefp = nil
+		writer = csv.NewWriter(os.Stdout)
+	}
+
+	fields := flag.Args()
+
+	// Write field names first
+	var fieldnames []string
+	for i := range fields {
+		fieldnames = append(fieldnames, fields[i])
+	}
+	writer.Write(fieldnames)
+
+	// For deduping, use this filter API:
+	// reader := adifparser.NewDedupeADIFReader(fp)
+
+	reader := adifparser.NewADIFReader(fp)
+	for record, err := reader.ReadRecord(); record != nil || err != nil; record, err = reader.ReadRecord() {
+		if err != nil {
+			if err != io.EOF {
+				fmt.Fprint(os.Stderr, err)
+			}
+			break // when io.EOF break the loop!
+		}
+		// Write a CSV record with chosen fields
+		newrecord := []string{}
+		for i := range fields {
+			newvalue, err := record.GetValue(fields[i])
+			if err != nil {
+				fmt.Fprint(os.Stderr, err)
+				break
+			}
+			newrecord = append(newrecord, newvalue)
+		}
+		writer.Write(newrecord)
+
+	}
+
+	// Flush and close the output
+	writer.Flush()
+	if writefp != os.Stdout {
+		writefp.Close()
+	}
+}
